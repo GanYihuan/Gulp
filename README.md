0fb029a7c8ee313f7626cd6ceb87bf491aa7ffab
GanYihuan <583520052@qq.com>
2018-11-05 15:48:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+﻿# gulp-learn
+
+## run
+
+```console
+gulp tastName
+```
+
+- gulp-less: 编译 less
+- gulp-jade: 编译 jade
+- gulp-connect: 创建本地服务器
+- gulp-concat: 合并文件
+- gulp-rename: 重命名文件
+- gulp-imagemin: 压缩图片
+- gulp-minify-html: 压缩 html
+- gulp-minify-css: 压 缩 css
+- gulp-uglify: 压缩 js
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+﻿const gulp = require('gulp')
+const less = require('gulp-less')
+const connect = require('gulp-connect')
+
+gulp.task('hello', function() {
+	console.log('hello gulp')
+})
+
+// copy file
+gulp.task('dest', function() {
+	// 所有文件 */*.*
+	// pipe: 文件走向下一节
+	gulp.src('src/*.html').pipe(gulp.dest('dist/'))
+})
+
+// default task
+gulp.task('default', function() {
+	console.log('default')
+	// 监视文件, 有变化执行对应任务
+	gulp.watch('src/*', ['dest'])
+})
+
+// less 转换为 less
+gulp.task('style', function() {
+	gulp
+		.src('src/**/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('dist/'))
+})
+
+// 监听 style 任务
+gulp.task('watch', function() {
+	gulp.watch('src/**/*.less', ['style'])
+})
+
+// 定义 http 服务器, 默认监听 8080
+gulp.task('serve', function() {
+	connect.server({
+		root: 'public',
+		// 文件变化刷新网页
+		livereload: true
+	})
+	gulp.watch('public/**/*.*', ['reload'])
+})
+
+gulp.task('reload', function() {
+	gulp.src('./**/*.*').pipe(connect.reload())
+})
